87c15c96a6beebaf505989f4bdb4f3d9ab09a410
Sanjida <sanjida.ice.778@gmail.com>
2024-10-08 22:28:49 +0600

    relevant text added

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ async function loadCategory() {
 }
 
 const showCategoryBtn = (data) => {
-  // console.log(data);
   const categoryBtnContainer = document.getElementById(
     "category-btn-container"
   );
 
   data.forEach((element) => {
-    // console.log(element.category_icon)
     const div = document.createElement("div");
     div.classList.add("border", "rounded-2xl");
     div.innerHTML = `
@@ -47,7 +45,6 @@ const loadSpinner = (category) => {
 function removeActive(){
  const removeAll = document.getElementsByClassName('category-btns');
  for(let item of removeAll){
-  // console.log(item);
   item.parentElement.classList.remove('border-primary', 'border-2', 'bg-primary', 'bg-opacity-10', 'rounded-[120px]')
   item.parentElement.classList.add('border', 'rounded-2xl');
  }
@@ -73,7 +70,6 @@ async function loadAllPets(status) {
     "https://openapi.programming-hero.com/api/peddy/pets"
   );
   const data = await res.json();
-  // console.log(data.pets)
   document.getElementById("loader").style.display = "block";
   document.getElementById("loader-container").style.display = "flex";
   const cardContainer = document.getElementById("pet-container-all");
@@ -92,11 +88,9 @@ async function loadAllPets(status) {
 }
 
 const sortBtn = (pets) => {
-  // console.log(pets);
   document.getElementById("loader").style.display = "none";
   document.getElementById("loader-container").style.display = "none";
   pets.sort((a, b) => b.price - a.price);
-  // console.log(pets)
   showAllPets(pets);
 };
 
@@ -124,8 +118,7 @@ const showAllPets = (pets) => {
     cardContainer.innerHTML = `
     <img src="images/error.webp"/>
     <h2 class = "font-bold text-3xl mt-6 mb-4">No Information Available</h2>
-    <p class = "text-tex_light text-center md:w-3/4 ">It is a long established fact that a reader will be distracted by the readable content of a page when looking at 
-its layout. The point of using Lorem Ipsum is that it has a.</p>
+    <p class = "text-tex_light xl:text-center xl:w-3/4 ">Sorry, no pets are currently available in this category. Please check back later or explore other categories to find your perfect companion.</p>
     `;
     return;
   } else {
@@ -143,12 +136,11 @@ its layout. The point of using Lorem Ipsum is that it has a.</p>
     );
   }
   pets.forEach((pet) => {
-    // console.log(pet)
     const div1 = document.createElement("div");
     div1.className = "p-5 border rounded-xl col-span-1";
     div1.innerHTML = `
         
-              <div class = "h-44"><img class = "rounded-lg w-full h-full object-cover" src=${pet.image}/></div>
+              <div class = "lg:h-44"><img class = "rounded-lg w-full h-full object-cover" src=${pet.image}/></div>
               <div class="space-y-2 py-6 border-b">
                 <h4 class="text-xl font-bold">${pet.pet_name}</h4>
                 <div class="flex items-center gap-2">
@@ -195,12 +187,10 @@ function modalCount(id) {
   const countContainer = document.getElementById("countdown");
   const count = document.getElementById("count");
   const modal = document.getElementById("my_modal_count");
-  // document.getElementById('count-modal-container').appendChild(modal);
 
   let num = 3;
   count.innerText = num;
   countContainer.classList.add("flex");
-  // document.getElementById(`adopt-${id}`).innerText = "Adopt";
 
   const clockId = setInterval(() => {
     num--;
@@ -208,7 +198,6 @@ function modalCount(id) {
     if (num == 1) {
       clearInterval(clockId);
     }
-    // console.log(num);
   }, 1000);
   setTimeout(() => {
     countContainer.classList.add("none");
@@ -219,7 +208,6 @@ function modalCount(id) {
 }
 
 async function displayModal(id) {
-  // console.log(id)
   const res = await fetch(
     `https://openapi.programming-hero.com/api/peddy/pet/${id}`
   );
@@ -228,7 +216,6 @@ async function displayModal(id) {
 }
 
 function loadModal(data) {
-  // console.log(data)
   const modalBox = document.getElementById("modal-box");
   modalBox.innerHTML = `
    <div>
@@ -283,7 +270,6 @@ function loadModal(data) {
 }
 
 async function loadLikedImage(id) {
-  // console.log(id)
   const res = await fetch(
     `https://openapi.programming-hero.com/api/peddy/pet/${id}`
   );
@@ -293,7 +279,6 @@ async function loadLikedImage(id) {
 }
 
 function showLikedImage(image) {
-  // console.log(image)
   const imgContainer = document.getElementById("show-liked-img");
   const div = document.createElement("div");
   div.className = "border rounded-xl col-span-1";
